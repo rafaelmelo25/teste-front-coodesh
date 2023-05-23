cd07d63abb6fc3d1c80439685a0fb287eebc1dce
rafaelmelo25 <rafael.melo.p@hotmail.com>
2023-05-22 22:02:00 -0300

    Criação layout para teste front-end

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Abrir modal de avaliação
+function openModal() {
+    document.getElementById("modal").style.display = "block";
+  }
+  
+  // Fechar modal de avaliação
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
